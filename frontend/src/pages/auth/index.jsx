8c9b9a36377ee1dcd2bc3cfce51d7efef809138a
Tirth02 <pateltirth0902@gmail.com>
2024-12-17 18:43:25 +0530

    routing implemented for all pages

---
+++ frontend/src/pages/auth/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+        hello auth
+    </div>
+  )
+}
+
+export default Auth
---
+++ frontend/src/pages/profile/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Auth from '../auth'
+const Profile = () => {
+  return (
+    <>
+        Profile
+    </>
+  )
+}
+
+export default Profile
