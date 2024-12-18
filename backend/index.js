3cb2095393edf39f295cfc3b64de075c54378411
Tirth02 <pateltirth0902@gmail.com>
2024-12-18 23:25:45 +0530

    backend server setup done

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import connectDB from "./utils/db.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET","POST","PUT","PATCH","DELETE"],
+    credentials:true
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port,() =>{
+    connectDB();
+    console.log(`Server ruuning at ${port}`);
+});
+
