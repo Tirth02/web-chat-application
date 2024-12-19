4aaa569665f765e457ef5ecda4319d257b0a7fe0
Tirth02 <pateltirth0902@gmail.com>
2024-12-19 13:54:27 +0530

    routing and controller logic for signup done in backend server

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import connectDB from "./utils/db.js";
+import authRoutes from "./routes/AuthRoutes.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
+app.use('/api/auth',authRoutes);
 const server = app.listen(port,() =>{
     connectDB();
     console.log(`Server ruuning at ${port}`);
