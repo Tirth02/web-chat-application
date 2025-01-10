6c5442d5dbe7e451056d014d3d31fa8a18045d7d
Tirth02 <pateltirth0902@gmail.com>
2025-01-10 20:59:08 +0530

    profile page setup done with api and functionality for back also implemented

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -4,35 +4,32 @@ import { compare } from "bcrypt";
 
 const maxAge = 1 * 24 * 60 * 60 * 1000;
 
-const createToken = (email,userId) =>{
-    return jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge});
+const createToken = (email, userId) => {
+    return jwt.sign({ email, userId }, process.env.JWT_KEY, { expiresIn: maxAge });
 }
-export const login = async(request,response,next) =>{
+export const login = async (request, response, next) => {
     try {
-        const{email,password} = request.body;
-        if(!email || !password)
-        {
+        const { email, password } = request.body;
+        if (!email || !password) {
             return response.status(400).send("Email and password is required");
         }
-        const user = await User.findOne({email});
-        if(!user)
-        {
+        const user = await User.findOne({ email });
+        if (!user) {
             return response.status(404).send("User with given email not found");
         }
-        const auth = await compare(password,user.password);
-        if(!auth)
-        {
+        const auth = await compare(password, user.password);
+        if (!auth) {
             return response.status(400).send("Password is incorrect");
         }
-        response.cookie("jwt",createToken(email,user.id),{
+        response.cookie("jwt", createToken(email, user.id), {
             maxAge,
-            secure:true,
-            sameSite:"None"
+            secure: true,
+            sameSite: "None"
         });
         return response.status(201).json({
-            user:{
+            user: {
                 id: user.id,
-                email:user.email,
+                email: user.email,
                 profileSetup: user.profileSetup,
                 firstName: user.firstName,
                 lastName: user.lastName,
@@ -46,24 +43,23 @@ export const login = async(request,response,next) =>{
     }
 }
 
-export const signup = async(request,response,next) =>{
+export const signup = async (request, response, next) => {
     try {
-        const{email,password} = request.body;
-        if(!email || !password)
-        {
+        const { email, password } = request.body;
+        if (!email || !password) {
             return response.status(400).send("Email and password is required");
         }
-        const user = await User.create({email,password});
-       
-        response.cookie("jwt",createToken(email,user.id),{
+        const user = await User.create({ email, password });
+
+        response.cookie("jwt", createToken(email, user.id), {
             maxAge,
-            secure:true,
-            sameSite:"None"
+            secure: true,
+            sameSite: "None"
         });
         return response.status(201).json({
-            user:{
+            user: {
                 id: user.id,
-                email:user.email,
+                email: user.email,
                 profileSetup: user.profileSetup
             },
         });
@@ -73,23 +69,50 @@ export const signup = async(request,response,next) =>{
     }
 }
 
-export const getUserInfo = async(request,response,next) =>{
+export const getUserInfo = async (request, response, next) => {
     try {
         const userData = await User.findById(request.userId);
-        if(!userData)
-        {
+        if (!userData) {
             return response.status(404).send("User with the given id not found.");
         }
-        
+
         return response.status(200).json({
-            
-                id: userData.id,
-                email:userData.email,
-                profileSetup: userData.profileSetup,
-                firstName: userData.firstName,
-                lastName: userData.lastName,
-                image: userData.image,
-                color: userData.color,
+
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
+        });
+    } catch (error) {
+        console.log(error);
+        return response.status(500).send("Internal server error");
+    }
+}
+
+export const updateProfile = async (request, response, next) => {
+    try {
+        const { userId } = request;
+        const { firstName, lastName, color } = request.body;
+
+        if (!firstName || !lastName) {
+            return response.status(400).send("FirstName, lastName and color is required.");
+        }
+
+        const userData = await User.findByIdAndUpdate(userId, {
+            firstName, lastName, color, profileSetup: true
+        }, { new: true, runValidators: true })
+        return response.status(200).json({
+
+            id: userData.id,
+            email: userData.email,
+            profileSetup: userData.profileSetup,
+            firstName: userData.firstName,
+            lastName: userData.lastName,
+            image: userData.image,
+            color: userData.color,
         });
     } catch (error) {
         console.log(error);
--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Auth from "../auth";
 import { IoArrowBack } from "react-icons/io5";
@@ -8,20 +8,73 @@ import { FaTrash, FaPlus } from "react-icons/fa";
 import { colors, getColor } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { apiClient } from "@/lib/api-client";
+import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
 const Profile = () => {
   const navigate = useNavigate();
-  const { userInfo } = useAppStore();
+  const { userInfo,setUserInfo } = useAppStore();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [image, setImage] = useState("");
   const [hovered, setHovered] = useState("");
   const [selectedColor, setSelectedColor] = useState(0);
 
-  const saveChanges = async () => {};
+  useEffect(() =>{
+    if(userInfo.profileSetup)
+    {
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName);
+      setSelectedColor(userInfo.color);
+    }
+  },[userInfo])
+ 
+  const validateProfile = () => {
+    if (!firstName) {
+      toast.error("First Name is required.");
+      return false;
+    }
+    if (!lastName) {
+      toast.error("Last Name is required.");
+      return false;
+    }
+    return true;
+  };
+  const saveChanges = async () => {
+    if (validateProfile()) {
+      try {
+        const response = await apiClient.post(
+          UPDATE_PROFILE_ROUTE,
+          { firstName, lastName, color: selectedColor },
+          { withCredentials: true }
+        );
+        if(response.status === 200 && response.data)
+        {
+          setUserInfo({...response.data});
+          toast.success("Profile updated successfully");
+          navigate("/chat");
+        }
+      } catch (error) {
+        toast.error(error);
+        console.log(error);
+      }
+    }
+  };
+
+  const handleNavigate = () =>{
+    if(userInfo.profileSetup)
+    {
+      navigate("/chat");
+    }
+    else
+    {
+      toast.error("Please setup profile.");
+    }
+  }
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className="flex flex-col gap-10 w-[80vw] md:w-max">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack className="text-4xl lg:text-6xl text-white/90 cursor-pointer " />
         </div>
         <div className="grid grid-cols-2">
@@ -107,8 +160,8 @@ const Profile = () => {
         </div>
         <div className="w-full">
           <Button
-           className="h-14 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300"
-           onClick={saveChanges}
+            className="h-14 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300"
+            onClick={saveChanges}
           >
             Save Changes
           </Button>
