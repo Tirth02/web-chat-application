ee7aee07adb6f9ab624a6314db844f12e472a414
Tirth02 <pateltirth0902@gmail.com>
2025-03-15 09:59:07 +0530

    tooltip for edit profile and name added from backend

--- frontend/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
+++ frontend/src/pages/chat/components/contacts-container/components/profile-info/index.jsx
@@ -1,9 +1,12 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { useAppStore } from "@/store";
 import { HOST } from "@/utils/constants";
-
+import {FiEdit2} from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
 const ProfileInfo = () => {
-    const {userInfo} = useAppStore();
+  const { userInfo } = useAppStore();
+  const navigate = useNavigate();
   return (
     <div className="absolute bottom-0 h-16 flex items-center justify-between px-10 w-full bg-[#2a2b33]">
       <div className="flex gap-3 items-center justify-center">
@@ -28,6 +31,21 @@ const ProfileInfo = () => {
             )}
           </Avatar>
         </div>
+        <div>
+          {userInfo.firstName && userInfo.lastName
+            ? `${userInfo.firstName} ${userInfo.lastName}`
+            : ""}
+        </div>
+      </div>
+      <div className="flex gap-5 ">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+                <FiEdit2 className="text-purple-500 text-xl font-medium" onClick={() => navigate("/profile")}/>
+            </TooltipTrigger>
+            <TooltipContent className="bg-[#1c1b1e] border-none text-white">Edit Profile</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   );
