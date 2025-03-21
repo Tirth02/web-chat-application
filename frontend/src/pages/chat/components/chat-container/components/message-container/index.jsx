f23c093e4300b85bc0c95e3baf6fe6c8cede2789
Tirth02 <pateltirth0902@gmail.com>
2025-03-22 01:44:42 +0530

    download file functionality done

--- frontend/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ frontend/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -43,8 +43,18 @@ const MessageContainer = () => {
     return imageRegex.test(filePath);
   }
 
-  const downloadFile = (file) =>{
-
+  const downloadFile = async(url) =>{
+    const response = await apiClient.get(`${HOST}/${url}`,{
+      responseType: "blob",
+    });
+    const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = urlBlob;
+    link.setAttribute("download",url.split("/").pop());
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(urlBlob);
   }
   const renderMessages = () => {
     let lastDate = null;
